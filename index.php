b58e07e8521de33d8bef87bf14703441185c88fb
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-02 22:04:00 +0200

    moved Movie instances to index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
 require_once __DIR__ . "/db.php";
+require_once __DIR__ . "/Models/Movie.php";
 
+$theNeverEndingStory = new Movie("The NeverEnding Story", "ENG", "1984", "94", "G");
+$watchmen = new Movie("Watchmen", "ENG", "2009", "163", "R");
+
+var_dump($theNeverEndingStory, $watchmen);
+
+echo $watchmen->setViewersAge(13);
 ?>
