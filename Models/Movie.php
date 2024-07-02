9daa00c55902d7dfc7a8fd99d278249b0f054c4e
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-02 18:22:47 +0200

    tested Movie class and method

--- Models/Movie.php
+++ Models/Movie.php
@@ -4,12 +4,30 @@ class Movie {
     public $language;
     public $releaseDate;
     public $runningTime;
+    public $rating;
 
-    public function __construct($_title, $_lang, $_releaseDate, $_runningTime){
+    public function __construct($_title, $_lang, $_releaseDate, $_runningTime, $_rating){
         $this->title = $_title;
         $this->language = $_lang;
         $this->releaseDate = $_releaseDate;
         $this->runningTime = $_runningTime;
+        $this->rating = $_rating;
     }
-
+    public function setViewersAge($age){
+        if($this->rating === "PG" || $this->rating === "PG-13"){
+            if($age <= 13){
+                echo "Viewing is recommended in the presence of an adult.";
+            };
+        } elseif ($this->rating === "R"){
+            if($age < 18){
+                echo "Prohibited for minors unaccompanied by an adult.";
+            };
+        } elseif ($this->rating === "NC-17"){
+            if($age < 18){
+                echo "Adults only.";
+            };
+        } else {
+            echo "All ages admitted.";
+        }
+    }
 }
--- db.php
+++ db.php
@@ -1,1 +1,9 @@
 <?php 
+require_once __DIR__ . "/Models/Movie.php";
+
+$theNeverEndingStory = new Movie("The NeverEnding Story", "ENG", "1984", "94", "G");
+$watchmen = new Movie("Watchmen", "ENG", "2009", "163", "R");
+
+var_dump($theNeverEndingStory, $watchmen);
+
+$watchmen->setViewersAge(13);
