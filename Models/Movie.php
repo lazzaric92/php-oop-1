22502130eb28a18d7fa0a75921384e03a55fd2d6
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-02 17:38:40 +0200

    WIP added Movie class

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,1 +1,15 @@
 <?php 
+class Movie {
+    public $title;
+    public $language;
+    public $releaseDate;
+    public $runningTime;
+
+    public function __construct($_title, $_lang, $_releaseDate, $_runningTime){
+        $this->title = $_title;
+        $this->language = $_lang;
+        $this->releaseDate = $_releaseDate;
+        $this->runningTime = $_runningTime;
+    }
+
+}
